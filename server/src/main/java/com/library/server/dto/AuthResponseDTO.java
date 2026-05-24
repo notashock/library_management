e7061e8df9467ed891963c276faa4ad630879e74
notashock <130152937+notashock@users.noreply.github.com>
2026-05-24 14:43:31 +0530

    modified response of auth service

--- server/src/main/java/com/library/server/dto/AuthResponseDTO.java
+++ server/src/main/java/com/library/server/dto/AuthResponseDTO.java
@@ -1,5 +1,6 @@
 package com.library.server.dto;
 
+import com.library.server.models.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,4 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthResponseDTO {
     private String token;
+//    private String name;
+    private String email;
+    private Role role;
 }
--- server/src/main/java/com/library/server/services/AuthService.java
+++ server/src/main/java/com/library/server/services/AuthService.java
@@ -30,7 +30,7 @@ public class AuthService {
                 .build();
         memberRepository.save(member);
         var jwtToken = jwtService.generateToken(member);
-        return AuthResponseDTO.builder().token(jwtToken).build();
+        return AuthResponseDTO.builder().email(member.getEmail()).role(member.getRole()).token(jwtToken).build();
     }
 
     public AuthResponseDTO authenticate(AuthRequestDTO request) {
@@ -40,6 +40,6 @@ public class AuthService {
         var member = memberRepository.findByEmail(request.getEmail())
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(member);
-        return AuthResponseDTO.builder().token(jwtToken).build();
+        return AuthResponseDTO.builder().email(member.getEmail()).role(member.getRole()).token(jwtToken).build();
     }
 }
